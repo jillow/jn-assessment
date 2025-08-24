716a2517f14c31569280ce7d5e5db68958ff63c7
James <james.a.c.naish@googlemail.com>
2025-08-24 11:43:37 +0100

    add: products with basic catalogue config and sizing data

---
+++ tests/test-data/products.ts
@@ -0,0 +1,10 @@
+export const CATALOGUE_CONFIG = {
+    EXPECTED_PRODUCT_COUNT: 16,
+};
+
+export const SIZE_FILTERS_TESTS = [
+    {
+        size: 'S',
+        expectedProducts: ['Black batman T-shirt', 'Blue Sweatshirt']
+    }
+] as const
