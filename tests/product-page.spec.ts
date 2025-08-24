71f1b717a09c0efab9506ec1a1e2be93d6368769
James <james.a.c.naish@googlemail.com>
2025-08-24 11:44:08 +0100

    add: test for single sizing filter

--- tests/product-page.spec.ts
+++ tests/product-page.spec.ts
@@ -1,5 +1,6 @@
 import { test, expect } from '@playwright/test';
 import { ProductPage } from './page-objects/ProductPage';
+import { CATALOGUE_CONFIG, SIZE_FILTERS_TESTS } from './test-data/products';
 
 test.describe('Product Page POM tests', () => {
     let productPage: ProductPage;
@@ -14,6 +15,16 @@ test.describe('Product Page POM tests', () => {
         test('should display correct product count on load', async ({ page }) => {
             const productCount = await productPage.getProductCount();
             expect(productCount).toBe(16); //get this from config later
+        });
+
+        test('should filter products by size correctly', async ({ page }) => {
+            const testCase = SIZE_FILTERS_TESTS[0]; // run all eventually
+            await productPage.filterBySize(testCase.size);
+
+            const filteredCount = await productPage.getProductCount();
+            expect(filteredCount).toBeLessThan(CATALOGUE_CONFIG.EXPECTED_PRODUCT_COUNT);
+            expect(filteredCount).toBeGreaterThan(0);
+            await expect(page.getByText(testCase.expectedProducts[0])).toBeVisible();
         });
     });
 });
